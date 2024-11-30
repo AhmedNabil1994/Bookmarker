e34cb1ca8ec153052ad4bf65b6e325fce5aeffe0
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-12-01 00:09:03 +0200

    complete validations

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+// DOM elements
 var bookmarkNameInput = document.getElementById("bookmarkNameInput");
 var bookmarkURLInput = document.getElementById("bookmarkURLInput");
 var tableBody = document.getElementById("tableBody");
-var duplicateModal = document.getElementById("duplicateModal");
+var duplicatePopup = document.querySelector(".duplicatePopup");
+var validationPopup = document.querySelector(".validationPopup");
+var btnAdd = document.getElementById("btnAdd");
+var closeIconValidationPopup = document.querySelector(
+  ".validationPopup span.fa-close"
+);
+var closeIconDuplicatePopup = document.querySelector(
+  ".duplicatePopup span.fa-close"
+);
+var popupBox = document.querySelector(".modal-dialog");
+// arrays
 var bookmarks = [];
 
 (function retrieveBookmarks() {
@@ -15,14 +26,18 @@ function createBookmark() {
   if (checkDuplicateName()) {
     return;
   }
-  var bookmark = {
-    name: bookmarkNameInput.value,
-    url: bookmarkURLInput.value,
-  };
-  bookmarks.push(bookmark);
-  setToLocalStorage("bookmarks", bookmarks);
-  displayBookmark(bookmarks);
-  clearForm();
+  if (validateForm(bookmarkNameInput) && validateForm(bookmarkURLInput)) {
+    var bookmark = {
+      name: bookmarkNameInput.value,
+      url: bookmarkURLInput.value,
+    };
+    bookmarks.push(bookmark);
+    setToLocalStorage("bookmarks", bookmarks);
+    displayBookmark(bookmarks);
+    clearForm();
+  } else {
+    validationPopup.classList.replace("d-none", "d-flex");
+  }
 }
 
 function displayBookmark(list) {
@@ -66,6 +81,12 @@ function deleteBookmark(index) {
 }
 
 function visitBookmark(index) {
+  if (
+    !bookmarks[index].url.startsWith("http://") &&
+    !bookmarks[index].url.startsWith("https://")
+  ) {
+    bookmarks[index].url = "https://" + bookmarks[index].url;
+  }
   window.open(bookmarks[index].url, "_blank");
 }
 
@@ -87,12 +108,65 @@ function checkDuplicateName() {
     if (
       bookmarks[i].name.toLowerCase() === bookmarkNameInput.value.toLowerCase()
     ) {
-      duplicateModal.classList.remove("d-none");
+      duplicatePopup.classList.remove("d-none");
       return true;
     }
   }
 }
 
+function validateForm(input) {
+  var regex = {
+    bookmarkNameInput: /^\w{3,}$/,
+    bookmarkURLInput:
+      /^(https?:\/\/(?:www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+  };
+  var isValid = regex[input.id].test(input.value);
+  if (isValid) {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+    btnAdd.classList.remove("disabled");
+    input.nextElementSibling.classList.replace("d-block", "d-none");
+  } else {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    btnAdd.classList.add("disabled");
+    input.nextElementSibling.classList.replace("d-none", "d-block");
+  }
+  if (
+    regex["bookmarkNameInput"].test(bookmarkNameInput.value) &&
+    regex["bookmarkURLInput"].test(bookmarkURLInput.value)
+  ) {
+    btnAdd.classList.remove("disabled");
+  } else {
+    btnAdd.classList.add("disabled");
+  }
+  return isValid;
+}
+
+closeIconValidationPopup.addEventListener("click", function () {
+  closePopup(validationPopup);
+});
+
+closeIconDuplicatePopup.addEventListener("click", function () {
+  closePopup(duplicatePopup);
+});
+
+validationPopup.addEventListener("click", function () {
+  closePopup(validationPopup);
+});
+
+popupBox.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
 function closePopup(popup) {
   popup.classList.add("d-none");
 }
+
+(function checkEmptyInputs() {
+  if (bookmarkNameInput.value !== "" && bookmarkURLInput.value !== "") {
+    btnAdd.classList.remove("disabled");
+  } else {
+    btnAdd.classList.add("disabled");
+  }
+})();
